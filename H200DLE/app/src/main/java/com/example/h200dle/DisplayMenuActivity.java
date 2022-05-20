9b59993ec14983e050175a9a475b2815a36916c7
shania smagh <smaghshania@gmail.com>
2022-05-19 19:19:59 -0700

    activity menu added

---
+++ H200DLE/app/src/main/java/com/example/h200dle/DisplayMenuActivity.java
@@ -0,0 +1,14 @@
+package com.example.h200dle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DisplayMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_menu);
+    }
+}
--- H200DLE/app/src/main/java/com/example/h200dle/MainActivity.java
+++ H200DLE/app/src/main/java/com/example/h200dle/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.h200dle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String MESSAGE = "com.example.H200DLE.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    /** called when user opens menu */
+    public void openMenu(View view) {
+
+        Intent intent = new Intent(this, DisplayMenuActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editTextTextPersonName2);
+        String message = editText.getText().toString();
+        intent.putExtra(MESSAGE, message);
+        startActivity(intent);
+    }
+
 }
